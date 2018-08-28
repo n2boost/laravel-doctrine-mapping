bd1afd6c12b3661b87d3af44b62670cd7e3511dd
lihao <lihao@n2boost.com>
2018-08-28 18:02:15 +0800

    fix config file error

--- config/laravel-doctrine-mapping.php
+++ config/laravel-doctrine-mapping.php
@@ -2,95 +2,25 @@
 
 return [
 
-    /*
-     * Mapping Config Engines.
-     * Can set to: yaml
-     */
+    'profile' => 'default',
 
     'mapping_type' => 'yaml',
 
-    /*
-     * Mapping config files dir
-     * full path will like this example: config/mappings/yaml/User.dcm.yml
-     */
-    'mapping_file_dir' => 'config/mappings',
-
-    'entities_file_dir' => 'resources/classes',
-
-    'profile' => 'local',
-    'isDevMode' => true,
-
-    'use_connection_pool' => 'laravel', // laravel, self
-    'connection' => 'mysql',
-
-    'connections' => [
-        'mysql' => [
-            'driver' => 'pdo_mysql',
-            'host' => '127.0.0.1',
-            'port' => 3306,
-            'user' => 'root',
-            'password' => '',
-            'dbname' => 'hunter',
-            'charset' => 'utf8mb4',
-            'collate' => 'utf8mb4_unicode_ci',
-        ]
-    ],
-
-    'active' => 'default',
-
     "profiles" => [
         'default' => [
-            'mapping_type' => 'yaml',
+            'mapping_file_dir' => 'config/mappings/default',
+            'entities_file_dir' => 'resources/classes/default',
 
-            'mapping_file_dir' => 'config/mapping-pools/juzicon',
-
-            'entities_file_dir' => 'resources/classes/juzicon',
-
-            'use_connection_pool' => 'laravel', // laravel, self
             'connection' => 'mysql',
-
-            'profile' => 'local',
-            'isDevMode' => true,
-
-            'connections' => [
-                'mysql' => [
-                    'driver' => 'pdo_mysql',
-                    'host' => '127.0.0.1',
-                    'port' => 3306,
-                    'user' => 'root',
-                    'password' => '',
-                    'dbname' => 'hunter',
-                    'charset' => 'utf8mb4',
-                    'collate' => 'utf8mb4_unicode_ci',
-                ]
-            ],
         ],
 
-        'n2boost' => [
-            'mapping_type' => 'yaml',
-
-            'mapping_file_dir' => 'config/mapping-pools/juzicon',
-
-            'entities_file_dir' => 'resources/classes/juzicon',
-
-            'use_connection_pool' => 'laravel', // laravel, self
-            'connection' => 'mysql',
-
-            'profile' => 'local',
+        'local' => [
             'isDevMode' => true,
 
-            'connections' => [
-                'mysql' => [
-                    'driver' => 'pdo_mysql',
-                    'host' => '127.0.0.1',
-                    'port' => 3306,
-                    'user' => 'root',
-                    'password' => '',
-                    'dbname' => 'hunter',
-                    'charset' => 'utf8mb4',
-                    'collate' => 'utf8mb4_unicode_ci',
-                ]
-            ],
+            'mapping_file_dir' => 'config/mappings/local',
+            'entities_file_dir' => 'resources/classes/local',
+
+            'connection' => 'n2boost_mysql',
         ]
     ]
 ];
